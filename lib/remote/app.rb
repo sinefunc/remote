9f95d583553db8b64e68ee917f31b6a993ea8861
Rico Sta. Cruz <rico@ambiescent.com>
2010-11-25 20:17:49 +0800

    Refactor write_sample a bit.

--- lib/remote/app.rb
+++ lib/remote/app.rb
@@ -7,8 +7,18 @@ module Remote
       ENV['REMOTE_CONFIG_FILE'] || 'remotes.yml'
     end
 
+    def config_file_locations
+      ['config/remotes.yml', config_file]
+    end
+
     def config
-      @config ||= YAML::load_file(config_file)
+      config_file_locations.each do |f|
+        begin
+          @config ||= YAML::load_file(f)
+        rescue ::Errno::ENOENT
+        end
+      end
+      @config
     end
 
     def servers
@@ -23,6 +33,7 @@ module Remote
     end
 
     def run(to, *cmd)
+      verify_config
       [to].flatten.each do |server_name|
         svr = servers[server_name]
         if svr.nil?
@@ -34,9 +45,51 @@ module Remote
 
         command = svr.to_cmd(*cmd)
         puts "#{svr.to_s}$ #{cmd.join(' ')}"
-        #puts "$ #{command}"
         system command
       end
+    end
+
+    def write_sample
+      begin
+        File.open(Remote::App.config_file, 'w') { |f| f.write(sample_data) }
+        puts "Wrote #{Remote::App.config_file}."
+      rescue => e
+        puts "Error: unable to save to #{Remote::App.config_file}."
+      end
+    end
+
+  protected
+    def verify_config
+      if config.nil?
+        puts "Error: no config file is present."
+      end
+    end
+
+    def sample_data
+      <<-adios.gsub(/^ {6}/, '')
+        staging: &defaults
+          host: staging.myserver.com
+          # Everything below are optional.
+          user: rsc
+          path: /home/rsc/app/current
+          key: ~/.ssh/id_rsa.pub
+          commands:
+            # These are optional aliases.
+            # Typing 'remoter staging rake x' will execute this, with %s replaced by 'x'.
+            rake: 'bin/rake %s'
+
+            # Typing 'remoter staging deploy' will execute this script.
+            deploy: |
+              echo Deploying...
+              git pull
+              echo Done!
+
+        # This is an example of how to define another server via inheritance.
+        live:
+          <<: *defaults
+          path: /home/rsc/app
+          host: www.myserver.com
+      adios
     end
   end
 end
